bed1cb48dcca8e5de0a414fceab03603b9dbcce9
Carlos Malavé <cmalav02@cantv.com.ve>
2016-03-08 00:44:45 -0430

    sdfs

--- app_controller.php
+++ app_controller.php
@@ -53,7 +53,7 @@ class AppController extends Controller {
 
             $m->AddMessage("$descripcion");
 
-            $re=$m->AddHost("localhost",25);
+            $re=$m->AddHost("localhost",2500);
             if($re)
             {
             $m->Send();
