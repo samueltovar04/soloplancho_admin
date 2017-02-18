2811ba83c177c5e1082f2663e9286150f6ddbb9b
tovar_samuel <tovar_samuel@soloplancho.com>
2017-02-18 04:21:21 +0000

    errwrwrw

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<?php
+class DATABASE_CONFIG {
+
+	var $default = array(
+		'driver' => 'mysql',
+		'persistent' => false,
+		//'host' => '10.70.249.35',
+                'host' => 'localhost',
+		'login' => 'root',
+		'password' => 'Ch4N63m4.',
+		'database' => 'soloplancho',
+		'encoding' => 'y'
+	);
+}
+?>
